827e508a5d8e6e121b863ff169ef1900ecb12bd1
theonogo <36893651+theonogo@users.noreply.github.com>
2018-03-03 00:23:05 +0100

    Updated Fog, created move func, started combat

--- kapture_dos/Character_movement.c
+++ kapture_dos/Character_movement.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h>              //TODO: replace the Map[][]=="x" to some fucking strcmp()   (and also fix all the struct refferences so they are consistent with Karsten's)
 #include <stdlib.h>
 
 int userinput(){       //WINDOWS VERSION
@@ -40,15 +40,25 @@ int userinput(){       //WINDOWS VERSION
 }
 }
 
-int posexist(int xpos,int ypos,int TEMPWIDTH,int TEMPHEIGHT){
+int posexist(int xpos,int ypos,int TEMPWIDTH,int TEMPHEIGHT){   //checks if a coordinate is within the map's borders
     if(xpos>=0 && ypos>=0 && xpos<TEMPWIDTH && ypos<TEMPHEIGHT){
         return 1;
     }
     return 0;
 }
 
+data_values move(int ypos,int xpos,int ydest,int xdest,data_values **Map){   //Moves a unit (TODO: see if Map works and if it does if its not too laggy)
+    Map[ydest][xdest].Team=Map[ypos][xpos].Team;
+    Map[ydest][xdest].Entity=Map[ypos][xpos].Entity;
+    Map[ydest][xdest].Flag=Map[ypos][xpos].Flag;
+    Map[ypos][xpos].Team=' ';
+    Map[ypos][xpos].Entity=' ';
+    Map[ypos][xpos].Flag=0;
+    return Map;
+}
+
 int main() {
-  int inputed,xpos,ypos,xmove,ymove,turnend,select,movend,movepoints,charend,movecost,TEMPWIDTH,TEMPHEIGHT,i,c;
+  int inputed,xpos,ypos,xmove,ymove,turnend,select,movend,movepoints,charend,movecost,TEMPWIDTH,TEMPHEIGHT,i,c,XTEMPSPAWN,YTEMPSPAWN;   //all TEMP variables are placeholders for now
     turnend=0;
     while (turnend==0){
 
@@ -112,20 +122,21 @@ int main() {
                 xmove++;
                 ymove--;
                 break;
-            default:
+            default://Fog Of War
                 break;
             }
             if(charend!=1){
                 if(movepoints>=movecost && posexist((xpos+xmove),(ypos+ymove),TEMPWIDTH,TEMPHEIGHT)==1){
                     if(Map[ypos+ymove][xpos+xmove].Entity==' '){
-                        if(Map[ypos][xpos].Flag=1){
-                            Map[ypos+ymove][xpos+xmove].Flag=1;
-                        }
+                        Map=move(ypos,xpos,ypos+ymove,xpos+xmovem,Map)
+                        /*
+                        Map[ypos+ymove][xpos+xmove].Flag=Map[ypos][xpos].Flag;
                         Map[ypos+ymove][xpos+xmove].Entity=Map[ypos][xpos].Entity;
                         Map[ypos+ymove][xpos+xmove].Team=Map[ypos][xpos].Team;
-                        Map[ypos][xpos].Team=0
+                        Map[ypos][xpos].Team=""
                         Map[ypos][xpos].Entity=' ';
                         Map[ypos][xpos].Flag=0;
+                        */
                         movepoint=movepoints-movecost;
                         ypos=ypos+ymove;
                         xpos=xpos+xmove;
@@ -136,28 +147,91 @@ int main() {
                         for(i=-1;i=<1;i=+2){
                             for(c=-1;c=<1;c=+2){
                                 if(posexist((xpos+i),(ypos+i),TEMPWIDTH,TEMPHEIGHT)==1){
-                                    Map[ypos+i][xpos+i].FOW=1;                //Fog Of War
+
+                                    if(Map[ypos][xpos].Team=="red"){        //Fog Of War: 0=unseen, 1=seen by red, 2=seen by blue, 3=seen by both
+                                        if(Map[ypos+i][xpos+i].FOW==2){
+                                            Map[ypos+i][xpos+i].FOW=3;
+                                        }
+                                        else{
+                                            Map[ypos+i][xpos+i].FOW=1;
+                                        }
+                                    }
+                                    else{
+                                        if(Map[ypos+i][xpos+i].FOW==1){
+                                            Map[ypos+i][xpos+i].FOW=3;
+                                        }
+                                        else{
+                                            Map[ypos+i][xpos+i].FOW=2;
+                                        }
+                                    }
 
                                     if(Map[ypos+i]T[xpos+i].Entity!=' ' && Map[ypos+i][xpos+i].Team!=Map[ypos][xpos].Team){ //FIGHT
-                                        goodguy=Map[ypos][xpos].Entity;
-                                        badguy=Map[ypos+i][xpos+i].Entity;
 
+
+                                        switch(Map[ypos][xpos].Entity) {        //TODO: make all combat, do a different combat if flag carrying
+                                        case "S":
+                                            switch(Map[ypos+i][xpos+i].Entity) {
+                                            case "I":
+
+
+                                                break;
+                                            case "T":
+
+                                                break;
+                                            case "F":
+
+                                                break;
+                                            }
+                                            break;
+
+
+                                        case "I":               //Maybe change I to C or something so it's more unique and different to T | TITITITITIITITIT <-- hard to see?
+                                            switch(Map[ypos+i][xpos+i].Entity) {
+                                            case "S":
+
+                                                break;
+                                            case "I":
+
+                                                break;
+                                            case "T":
+
+                                                break;
+                                            case "F":
+
+                                                break;
+                                            }
+                                            break;
+
+
+                                        case "T":
+                                            switch(Map[ypos+i][xpos+i].Entity) {
+                                            case "S":
+
+                                                break;
+                                            case "I":
+
+                                                break;
+                                            case "T":
+
+                                                break;
+                                            case "F":
+
+                                                break;
+                                            }
+                                            break;
+                                        }
                                     }
                                 }
                             }
                         }
-
-
-
+                        else{
+                        //INVALID MOVEMENT
+                        }
                     }
                     else{
                     //INVALID MOVEMENT
                     }
                 }
-                else{
-                //INVALID MOVEMENT
-                }
-
             }
         }
     }
