063f3c96687306a7b5fd24b4ea05c51c14c6ef0d
nerstak <33179821+nerstak@users.noreply.github.com>
2018-03-16 09:20:39 +0100

    Remove useless comment

--- kapture_dos/map_creator.py
+++ kapture_dos/map_creator.py
@@ -32,36 +32,3 @@ for i in range(line):
 print("Done.")
 
 fil.close()
-
-
-
-""" for i in range(int(line)):
-    redo=1
-    while redo:
-        redo=0
-        print("\n"+" "*56,end='')
-        print("1,2,3,4,5,6,7,8,9,10       ,15       ,20       ,25       ,30       ,35       ,40       ,45       ,50       ,45       ,50")
-        for j in range(i):
-            print(" "*56,end='')
-            for k in big_array[j+1]:
-                print(k+",",end='')
-            print()
-        new_line_str=str(input("Enter the '"+str(i)+"' line (with g/w/t and separated by comma): "))
-        new_line=new_line_str.split(",")
-        if len(new_line)!=int(column):
-            redo=1
-            print("Incrorrect number of columns")
-        for k in new_line:
-            if k not in ['g','w','t']:
-                redo=1
-                print("Incorrect number")
-    big_array.append(new_line)
-    for k in new_line:
-        if k=='g':
-            file.write("grass,")
-        elif k=='w':
-            file.write("water,")
-        elif k=='t':
-            file.write("tree,")
-    file.write("\n") """
-
